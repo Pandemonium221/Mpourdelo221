1086c852e0488b90df8bf178029cef37ae65c1ed
Pandemonium221 <115405782+Pandemonium221@users.noreply.github.com>
2022-10-09 15:31:23 +0300

    Add files via upload

---
+++ Mpourdelo.py
@@ -0,0 +1,27 @@
+print("Hello, welcome to Pandemonium Brothel")
+name = input("Pws se lene?\n")
+if name == "Porth":
+    print("Porth pare ton mpoulo, tha meineis agamhtos mia zwh")
+    exit()
+else:
+    print("Hello " + name + ", kalwsorisate sto tapeino moy mpoyrdelo.")
+#porti nothing personal kid
+gynaika = "Latines,Asiatises,Ebony,Teen,Milf,Gilf"
+print(name +", ti tha thelate apo tis shmerines mas poytanes? Shmera exoume diathesimes.\n" + gynaika)
+print("Parallhla ofeilw na epishmanw pws h diarkeia twn yphresiwn den lambanwntai ypopsin sthn telikh timh")
+paraggelia = input()
+plhthos= input("Poses poutanes tha thelate?\n")
+if 3 <= int(plhthos):
+    print("Oh boy exeis kefia shmera blepw")
+else:
+    print("Ok arketa tapeino se briskw")
+kostos = 30
+synolika = kostos * int(plhthos)
+print("Eyxaristw poly. To synoliko kostos einai: $" + str(synolika))
+print("Teleia " + name + ",tha exoume ta poytanakia etoima gia sena se 2 lepta.")
+
+
+
+
+
+
